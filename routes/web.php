443c2502c22f1382e128147dc4a2c047a145573e
Andre Bernardi <anjobernardi@msn.com>
2023-08-10 21:57:12 -0300

    testing bug fix por / route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -16,15 +17,16 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Auth/Login', [
-        'canResetPassword' => Route::has('password.request'),
-        'status' => session('status'),
-    ]);
-});
-
 Route::middleware('auth')->group(function () {
     //Route::get('/', [\App\Http\Controllers\DashboardController::class, "index"])->name('dashboard.index');
+    Route::get('/', function () {
+        return Inertia::render('Dashboard',[
+            [
+                'canLogin' => Route::has('login'),
+                'canRegister' => Route::has('register')
+            ]
+        ]);
+    })->middleware(['auth', 'verified'])->name('dashboard.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');

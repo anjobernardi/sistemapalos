4c5d0804547c65c0613f1dfc7b8df17aab0e56f8
Andre Bernardi <anjobernardi@msn.com>
2023-08-10 22:11:04 -0300

    testing bug fix por / route

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,11 @@ use Inertia\Inertia;
 */
 
 Route::middleware('auth')->group(function () {
-    //Route::get('/', [\App\Http\Controllers\DashboardController::class, "index"])->name('dashboard.index');
+    
     Route::get('/', function () {
-        return Inertia::render('Dashboard',[
-            [
-                'canLogin' => Route::has('login'),
-                'canRegister' => Route::has('register')
-            ]
-        ]);
-    })->middleware(['auth', 'verified'])->name('dashboard.index');
+        phpinfo();
+    })->middleware(['auth', 'verified'])->name('home');
+    Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, "index"])->name('dashboard.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
